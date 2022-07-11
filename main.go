7a3fb9067422c3fd6fd46776794703a0f99bd803
kartien <kevinramon523@gmail.com>
2022-07-10 23:27:31 -0500

    Create models or ORM

--- main.go
+++ main.go
@@ -5,12 +5,16 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/kartien/go-gorm-restapi/db"
+	"github.com/kartien/go-gorm-restapi/models"
 	"github.com/kartien/go-gorm-restapi/routes"
 )
 
 
 func main() {
 	db.DBConnection()
+
+	db.DB.AutoMigrate(models.Task{})
+	db.DB.AutoMigrate(models.User{})
 
 	r := mux.NewRouter()
 
---
+++ models/Task.go
@@ -0,0 +1,13 @@
+package models
+
+import "gorm.io/gorm"
+
+type Task struct {
+	gorm.Model
+
+	Title 		string `gorm:"not null; unique_idenx"`
+	Description string
+	Done 		bool `gorm:"default:false"`
+	UserID 		uint
+
+}
