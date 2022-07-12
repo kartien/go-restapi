2c037fcece831d5d2fc807ec3d59f7d4f5c13959
kartien <kevinramon523@gmail.com>
2022-07-11 21:15:27 -0500

    Create func Task

--- main.go
+++ main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"net/http"
-
 	"github.com/gorilla/mux"
 	"github.com/kartien/go-gorm-restapi/db"
 	"github.com/kartien/go-gorm-restapi/models"
 	"github.com/kartien/go-gorm-restapi/routes"
 )
-
 
 func main() {
 	db.DBConnection()
@@ -19,14 +17,18 @@ func main() {
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", routes.HomeHandler)
-	
+	//Users 
 	r.HandleFunc("/users", routes.GetrUsersHandler).Methods("GET")
 	r.HandleFunc("/users/{id}", routes.GetrUserHandler).Methods("GET")
 	r.HandleFunc("/users", routes.PostUserHandler).Methods("POST")
 	r.HandleFunc("/users/{id}", routes.DeleteUserHandler).Methods("DELETE")
+	//Tasks 
+	r.HandleFunc("/tasks", routes.GetTasksHandler).Methods("GET")
+	r.HandleFunc("/tasks/{id}", routes.GetTasksHandler).Methods("GET")
+	r.HandleFunc("/tasks", routes.PostTaskHandler).Methods("POST")
+	r.HandleFunc("/tasks/{id}", routes.DeleteTaskHandler).Methods("DELETE")
 
 	http.ListenAndServe(":3000", r)
-
 }
 
 
--- routes/tasks.routes.go
+++ routes/tasks.routes.go
@@ -1,3 +1,61 @@
 package routes
 
+import (
+	"encoding/json"
+	"net/http"
 
+	"github.com/gorilla/mux"
+	"github.com/kartien/go-gorm-restapi/db"
+	"github.com/kartien/go-gorm-restapi/models"
+)
+
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request){
+	var tasks []models.Task
+	db.DB.Find(&tasks)
+	json.NewEncoder(w).Encode(&tasks)
+}
+
+func GetTaskHandler(w http.ResponseWriter, r *http.Request){
+	var task models.User
+	params := mux.Vars(r)
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Task not found"))
+		return
+	}
+	json.NewEncoder(w).Encode(&task)
+}
+
+func PostTaskHandler(w http.ResponseWriter, r *http.Request){
+	var task models.Task
+
+	json.NewDecoder(r.Body).Decode(&task)
+
+	createTask := db.DB.Create(&task)
+	err := createTask.Error
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest) // 400 
+		w.Write([]byte(err.Error()))
+	}
+
+	json.NewEncoder(w).Encode(&task)
+
+	w.Write([]byte("Post"))
+}
+
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request){
+	var task models.Task
+	params := mux.Vars(r)
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Task not found"))
+		return
+	}
+	db.DB.Delete(&task)
+	w.WriteHeader(http.StatusOK)
+}
